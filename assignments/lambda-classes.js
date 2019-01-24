9fc10007f31683c874158b73b75865a87d277982
Mykol Benning <msmykolbenning@gmail.com>
2019-01-24 15:32:21 -0500

    created objects

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,97 @@
 // CODE here for your Lambda Classes
+
+//start of Person
+
+
+//Declare class keyword
+
+// Create a new constructor
+
+//Methods
+
+//end of class 
+
+
+
+
+//start of Instructor
+
+//declare class keyword
+
+//create a new constructor
+ 
+//Methods
+
+//end of class
+
+
+
+//start of Student 
+
+//Declare class keyword
+
+//Create a new constructor
+
+
+//Objects
+
+
+const Josh = new Instructor({
+    name: 'Josh',
+    location: 'California',
+    age: '35',
+    gender: 'male',
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    Phrase: 'The big boss',
+});
+
+const Rose = new Instructor({
+    name: 'Rose',
+    location: 'DC',
+    age: '28',
+    gender: 'female',
+    favLanguage: 'React',
+    specialty: 'Back-end',
+    Phrase: 'Just do it',
+});
+
+const Piper = new Student({
+    name: 'Piper',
+    location: 'Barcelona',
+    age: '20',
+    gender: 'female',
+    favLanguage: 'Python',
+    specialty: 'back-end',
+    Phrase: 'Coachella',
+});
+
+const Siri = new Student({
+    name: 'Siri',
+    location: 'Paris',
+    age: '30',
+    gender: 'male',
+    favLanguage: 'Java',
+    specialty: 'back-end',
+    Phrase: 'That is stupid',
+});
+
+const Caleb = new ProjectManager({
+    name: 'Caleb',
+    location: 'NY',
+    age: '25',
+    gender: 'male',
+    favLanguage: 'Ruby',
+    specialty: 'back-end',
+    Phrase: 'You can do it',
+});
+
+const Oly = new ProjectManager({
+    name: 'Oly',
+    location: 'DR',
+    age: '30',
+    gender: 'female',
+    favLanguage: 'PHP',
+    specialty: 'back-end',
+    Phrase: 'That looks good',
+});
